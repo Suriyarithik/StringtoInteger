ffdedb2d88595b9c3795536c33c88308edd2498c
Suriyarithik <suriyavedsum@gmail.com>
2017-07-05 22:11:44 +0530

    Create StringtoInteger.java

---
+++ StringtoInteger.java
@@ -0,0 +1,15 @@
+# StringtoInteger
+import java.io.*;
+import java.util.*;
+public class StringtoInteger
+{
+ public static void main(String args[])throws IOException
+ {
+   Scanner sc=new Scanner(System.in);
+   System.out.println("Enter the number:");
+   String a=sc.nextLine();
+   int n=Integer.parseInt(a);
+   System.out.println(n);
+   }
+   }
+   
